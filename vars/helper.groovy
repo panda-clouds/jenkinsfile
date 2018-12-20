6eb29a56715592af89d762b07c7f2cd9b5360216
Marc Smith <mr.marcsmith@me.com>
2018-12-19 17:51:49 -0700

    Create helper.groovy

---
+++ vars/helper.groovy
@@ -0,0 +1,46 @@
+def call() {
+pipeline {
+  agent any
+  tools {
+    nodejs 'Node 8.13.0'
+  }
+  environment {
+    NPM_TOKEN = credentials('npm-mrmarcsmith')
+  }
+  stages {
+    stage('Test') {
+      steps {
+        sh 'npm install'
+        sh 'npm test spec/*.spec.js'
+      }
+    }
+    stage('if master') {
+      when { branch "master" }
+      
+      stages {
+        stage('Deploy') {
+          input {
+            message "✅ All Unit tests passed!"
+            ok "Submit"
+            parameters {
+              choice(name: 'DEPLOY_TO_NPM', choices: ['No, Skip Deploy', 'Yes, Deploy'], description: 'Push to npm?')
+            }
+          }
+          
+          when { environment name: 'DEPLOY_TO_NPM', value: 'Yes, Deploy' }
+
+          steps {
+            sh 'npm publish --access=public'
+          }
+        }
+      }
+    }
+  }
+
+  post{
+    always {
+      deleteDir()
+    }
+  }
+}
+}
